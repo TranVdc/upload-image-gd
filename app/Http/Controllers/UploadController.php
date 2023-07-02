cbf5b6741b0139ad8689ec83c706fb6e5db273a8
DESKTOP-EP0674U\Admin <tranvanduoc2394@gmail.com>
2023-07-02 18:47:38 +0700

    upload image using GD

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -7,6 +7,7 @@ use Input;
 use Validator;
 use Redirect;
 use View;
+use Image;
 
 class UploadController extends Controller {
 
@@ -15,15 +16,36 @@ class UploadController extends Controller {
 	}
 
 	public function upload(Request $request) {
+
+
+        // this is normal upload
 		// $this->validate($request, [
 	    // 	'image' => 'mimes:jpeg,bmp,png,heic', //only allow this type extension file.
 		// ]);
 		
 		// 
-		$file = $request->file('image');
-		$fileName = time() . '_' . uniqid() . '.' . $file->getClientOriginalExtension();
+		// $file = $request->file('image');
+		// $fileName = time() . '_' . uniqid() . '.' . $file->getClientOriginalExtension();
 
-		$file->move('uploads', $file->getClientOriginalName()); 
+		// $file->move('uploads', $file->getClientOriginalName()); 
+        
+        // end normal upload
+
+        
+        // this is gd upload and resize
+        $file = $request->file('image');
+		$fileName = time() . '_' . uniqid() . '.' . $file->getClientOriginalExtension();
+        $file->move('uploads/original/', $fileName); 
+
+        $originalImagePath = public_path('uploads/original/' . $fileName);
+        $resizedImage = Image::make($originalImagePath)->resize(800, 600);
+
+        // Resize the image
+        $resizedImagePath = public_path('uploads/resized/' . $fileName);
+        // Save the resized image
+        $resizedImage->save($resizedImagePath);
+        // end gd upload and resize
+
 
 		echo 'Image Uploaded Successfully';
 	}
